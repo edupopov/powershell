9be4543cbadd4662f4eeafe714cd6932c29eea80
Eduardo Popovici <32421250+edupopov@users.noreply.github.com>
2026-02-02 09:48:57 -0300

    Update DriverImport.bat

--- DriverImport.bat
+++ DriverImport.bat
@@ -1,9 +1,12 @@
-REM Este script foi criaro para importar drivers para dentro da pasta C:\Windows\System32\DriverStore
-REM A Microsoft exige o uso de ferramentas como pnputil ou dism, que validam e registram os drivers corretamente.
-REM Script criado por Eduardo Popovici
+REM Scrip Criado pelo Eduardo Popovici
 REM /add-driver → adiciona o driver ao Driver Store
 REM C:\DriversBKP\*.inf → aponta para os arquivos .inf
 REM /subdirs → percorre todas as subpastas
 REM /install → já tenta instalar o driver no dispositivo compatível
+REM Onde os drivers ficam depois - C:\Windows\System32\DriverStore\FileRepository
+REM Nunca copie arquivos manualmente para essa pasta - Isso pode corromper o Driver Store e causar erros de inicialização
+REM Restaurar drivers após formatação - pnputil /add-driver
+REM Instalar drivers automaticamente - pnputil /add-driver /install
+REM Imagem offline / WIM - dism /add-driver
 
 pnputil /add-driver C:\DriversBKP\*.inf /subdirs /install
